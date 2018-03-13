3bc1075575169f6c111f603a1572c3ed24db029b
lijx <lijx@mo9.com>
2018-03-13 15:19:30 +0800

    完成托单事件发布

---
+++ src/main/java/com/entrusts/service/TradePairService.java
@@ -0,0 +1,31 @@
+package com.entrusts.service;
+
+import com.entrusts.mapper.TradePairMapper;
+import com.entrusts.module.entity.TradePair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by jxli on 2018/3/9.
+ */
+@Service
+@Transactional(readOnly = true)
+@CacheConfig(cacheNames="TradePair")
+public class TradePairService extends BaseService {
+	@Autowired
+	TradePairMapper tradePairMapper;
+
+	/**
+	 * @Description 查询交易对, 使用本地缓存
+	 * @param baseCurrencyAlias 基础货币
+	 * @param targetCurrencyAlias 目标货币
+	 * @return com.entrusts.module.entity.TradePair
+	 */
+	@Cacheable(key = "#baseCurrencyAlias+'_'+#targetCurrencyAlias")
+	public TradePair findTradePairByCoinName(String baseCurrencyAlias, String targetCurrencyAlias){
+		return tradePairMapper.findTradePairByCoinName(baseCurrencyAlias,targetCurrencyAlias);
+	}
+}
--- src/test/java/com/tmp/JedisTest.java
+++ src/test/java/com/tmp/JedisTest.java
@@ -18,38 +18,40 @@ import redis.clients.jedis.JedisPool;
 @SpringBootTest(classes = EntrustsApplication.class)
 @EnableAutoConfiguration
 public class JedisTest {
-	
+
 	@Autowired
 	private JedisPool jedisPool;
-	
+
 	@Test
 	public void redisTest() {
 		Jedis j = jedisPool.getResource();
 		j.set("test", "123");
 		System.out.println(j.get("test"));
 		j.close();
+		System.out.println(RedisUtil.get("DELEGATE_REQUEST_TOKEN_PREFIX"));
+		System.out.println(null == RedisUtil.get("DELEGATE_REQUEST_TOKEN_PREFIX"));
 	}
 
 	private static final String key = "ceshi";
 
 	@Test
 	public void redisLockTest() {
-		Runnable runnable = ()-> {
+		Runnable runnable = () -> {
 			String requestId = UUID.randomUUID().toString();
-			System.out.println(Thread.currentThread().getId()+","+ "准备获取锁");
-				boolean result = RedisUtil.tryLock(key,requestId);
-				System.out.println(Thread.currentThread().getId()+","+ result);
-				try {
-					Thread.sleep(20*1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				if (result){
-					RedisUtil.releaseLock(key,requestId);
-					System.out.println(Thread.currentThread().getId()+","+ "释放锁");
-				}
+			System.out.println(Thread.currentThread().getId() + "," + "准备获取锁");
+			boolean result = RedisUtil.tryLock(key, requestId);
+			System.out.println(Thread.currentThread().getId() + "," + result);
+			System.out.println(Thread.currentThread().getId() +"乱释放锁" + RedisUtil.releaseLock(key, "不存在的RequestId"));
+			try {
+				Thread.sleep(20 * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if (result) {
+				RedisUtil.releaseLock(key, requestId);
+				System.out.println(Thread.currentThread().getId() + "," + "释放锁");
+			}
 		};
-
 
 		Thread thread1 = new Thread(runnable);
 		Thread thread2 = new Thread(runnable);
