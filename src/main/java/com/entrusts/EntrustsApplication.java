d68e6682a3afad96bb098aa67489dd340800ad71
sxu <sxu@windows10.microdone.cn>
2018-03-15 18:24:34 +0800

    注册中心

--- src/main/java/com/entrusts/EntrustsApplication.java
+++ src/main/java/com/entrusts/EntrustsApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@MapperScan("com.entrusts.mapper")
 @EnableCaching
+@EnableDiscoveryClient
+@MapperScan("com.entrusts.mapper")
 public class EntrustsApplication {
 
 	public static void main(String[] args) {
