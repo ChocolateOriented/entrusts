9df5e24e16fb6dc04fba27535e77e5308e1062aa
cyuan <cyuan@mo9.com>
2018-04-02 20:32:21 +0800

    添加consumes

--- src/main/java/com/entrusts/manager/DealmakingClient.java
+++ src/main/java/com/entrusts/manager/DealmakingClient.java
@@ -2,6 +2,7 @@ package com.entrusts.manager;
 
 import com.entrusts.module.dto.DelCancelOrder;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "${feignClient.dealmaking.name}",path = "${feignClient.dealmaking.containerName}")
 public interface DealmakingClient {
     String DEAL_MAKING_PATH = "/api/bull/v1/dealmaking/";
-    @RequestMapping(value = DEAL_MAKING_PATH+"cancel_order",produces = "application/json",method = RequestMethod.POST)
+    @RequestMapping(value = DEAL_MAKING_PATH+"cancel_order",produces = MediaType.APPLICATION_JSON_UTF8_VALUE,consumes = "application/json",method = RequestMethod.POST)
     String delCancelOrder(String delCancelOrder);
 
 }
